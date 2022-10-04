acd0eb977bcdc397b6099206ebfeae1d4081537f
wangzhizhong <1010862019@qq.com>
2022-10-04 19:46:02 +0800

    1

--- 自定义运行-过滤条件搜索.py
+++ 自定义运行-过滤条件搜索.py
@@ -1,22 +1,19 @@
 def bigquant_run(bq_graph, inputs):
-    factor_pool = ['return_0>1.03',
-                   'volume_0/volume_1>1.7',
+    factor_pool = ['volume_0/volume_1>1.7',
                    'turn_0==max_turn',
                    'ac0==max_ac',
                    'sma_close_5>sma_close_10',
                    'sma_close_5>sma_close_20',
                    'sma_volume_5>sma_volume_10',
-                   'sma_volume_5>sma_volume_20',
                    'open_0<close_0',
                    'mf_net_pct_main_0<0.4',
                    'sum_mf>0.1',
                    'rank_return_0>0.9',
                    'rank_return_5>0.9',
-                   'rank_avg_mf_net_amount_5<0.5',
-                   'shangying<0.04']
+                   'rank_avg_mf_net_amount_5<0.5']
 
     parameters_list = [{'parameters': {'m10.expr': '&'.join(factor_pool[:])}}]
-    minus_list = [[]]
+    minus_list = ['&'.join(factor_pool[:])]
     for feature in factor_pool:
         tmp = factor_pool[:]
         tmp.remove(feature)
@@ -35,6 +32,7 @@ def bigquant_run(bq_graph, inputs):
     print("minus_list:", minus_list)
     results = T.parallel_map(run, parameters_list, max_workers=2, remote_run=False, silent=False)  # 任务数 # 是否远程#
     return results, minus_list
+
 
 
 
